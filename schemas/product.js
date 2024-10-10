a98e677826f119b5ca3753b704d5080a7fce7308
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-10-10 02:40:31 +0200

    Update product schema

--- schemas/product.js
+++ schemas/product.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  name: { type: String, required: [true, 'Item name is required'] },
-  active: { type: Boolean, default: true },
-  category: { type: String, required: [true, 'Category is required'] },
-  price: { type: Number, required: [true, 'Price is required'] },
-  courier: { type: String, required: [true, 'Courier is required'] },
-  packed: { type: Boolean, default: false },
+  name: { type: String, required: [true, 'Item name is required'] }, // DONE
+  active: { type: Boolean, default: true }, // DONE
+  category: { type: String, required: [true, 'Category is required'] }, // DONE
+  price: { type: Number, required: [true, 'Price is required'] }, // DONE
   colors: [{ type: Schema.Types.ObjectId, ref: 'DressColor' }]
 });
 
 module.exports = mongoose.model("Item", ItemSchema);
+
+  // OVA DVA IDU U PORUDZBINU !!!
+  // courier: { type: String, required: [true, 'Courier is required'] }, 
+  // packed: { type: Boolean, default: false },
