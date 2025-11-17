d455b76ee413f9c4d01174b89b6d0f0fedc02c79
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2025-11-17 11:06:21 +0100

    fix automatic database backup

--- schedulers/database/backupDatabase.js
+++ schedulers/database/backupDatabase.js
@@ -62,16 +62,16 @@ async function backupDatabase() {
 }
 
 function scheduleDatabaseBackup() {
-  // const scheduleTimes = ['0 4 * * *'];
-  // scheduleTimes.forEach((time) => {
-  //   cron.schedule(time, backupDatabase, {
-  //     timezone: 'Europe/Belgrade',
-  //   });
-  // });
+  const scheduleTimes = ['0 4 * * *'];
+  scheduleTimes.forEach((time) => {
+    cron.schedule(time, backupDatabase, {
+      timezone: 'Europe/Belgrade',
+    });
+  });
 
-  setTimeout(() => {
-    backupDatabase();
-  }, 20000);
+  // setTimeout(() => {
+  //   backupDatabase();
+  // }, 20000);
 }
 
 module.exports = { scheduleDatabaseBackup };
